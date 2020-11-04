8f1fc2238709db0868ea38aaff0cf48320901804
740491 <740491@unizar.es>
2020-11-04 17:12:06 +0100

    added the function in the same query

--- app/src/main/java/com/gasmap/app/repository/GasRepository.java
+++ app/src/main/java/com/gasmap/app/repository/GasRepository.java
@@ -16,7 +16,8 @@ public interface GasRepository extends CrudRepository<Gas, Integer>{
     @Query(value = "SELECT * FROM ZZGas WHERE latitude_gas =?1 AND longitude_gas = ?2", nativeQuery = true)
     public Gas findByLatAndLon(double lat, double lon);
 
-    @Query(value = "SELECT * FROM ZZGas WHERE gps.fnCalcDistanceKM(latitude_gas,?1,longitude_gas,?2) < 50.0 ", nativeQuery = true)
+    //@Query(value = "SELECT * FROM ZZGas WHERE gps.fnCalcDistanceKM(latitude_gas,?1,longitude_gas,?2) < 50.0 ", nativeQuery = true)
+    @Query(value = "SELECT * FROM ZZGas WHERE (ACOS(SIN(PI()*latitude_gas/180.0)*SIN(PI()*?1/180.0)+COS(PI()*latitude_gas/180.0)*COS(PI()*?1/180.0)*COS(PI()*?2/180.0-PI()*longitude_gas/180.0))*6371) < 50.0", nativeQuery = true)
     public Gas[] findByDistance(double lat2, double lon2);
 
     @Query(value = "SELECT * FROM ZZGas", nativeQuery = true)
